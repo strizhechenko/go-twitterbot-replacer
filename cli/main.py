30894e1cafac5a21540dd40636a17e96c798e226
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-03-16 13:12:05 +0500

    Only uniq tweets are shown.

--- cli/main.py
+++ cli/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import requests
-for line in requests.get('http://127.0.0.1:8080/tweets').json():
+for line in set([l.strip() for l in requests.get('http://127.0.0.1:8080/tweets').json()]):
     print line.encode('utf-8')
     print
